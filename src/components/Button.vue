ad22aa4cee9c9f273f8aaf2ae4e63982f3b01e51
manohySr <manohyrajaonah@gmail.com>
2024-10-20 20:53:44 +0400

    add: 	Button and Input components

---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    default: "Boutton",
+  },
+});
+</script>
+<template>
+  <button
+    class="bg-blue-900 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-md w-full focus:outline-none focus:shadow-outline"
+    type="submit"
+  >
+    {{ text }}
+  </button>
+</template>
