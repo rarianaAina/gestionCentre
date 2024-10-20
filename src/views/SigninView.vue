237b98cde471283293089898be9770d145eed26f
manohySr <manohyrajaonah@gmail.com>
2024-10-20 18:31:42 +0400

    WIP: signin view

--- src/views/SigninView.vue
+++ src/views/SigninView.vue
@@ -1,1 +1,34 @@
-<template>Signin</template>
+<template>
+  <div class="mb-4">
+    <label class="block text-gray-700 font-bold mb-2">
+      Nom d'utilisateur
+    </label>
+    <input
+      type="text"
+      id="location"
+      name="location"
+      class="border rounded w-full py-2 px-3 mb-2"
+      placeholder="Rariana beaugosse"
+      required
+    />
+  </div>
+  <div class="mb-4">
+    <label class="block text-gray-700 font-bold mb-2"> Mot de passe </label>
+    <input
+      type="text"
+      id="location"
+      name="location"
+      class="border rounded w-full py-2 px-3 mb-2"
+      placeholder="Password"
+      required
+    />
+  </div>
+  <div>
+    <button
+      class="bg-blue-900 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-lg w-full focus:outline-none focus:shadow-outline"
+      type="submit"
+    >
+      Se connecter
+    </button>
+  </div>
+</template>
