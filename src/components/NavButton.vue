78cba16e3b9f629cf7de39093cf74c01ac4e867b
manohySr <manohyrajaonah@gmail.com>
2024-11-01 14:59:53 +0400

    add:    navigation button

---
+++ src/components/NavButton.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    default: "Boutton", // Default button text
+  },
+  bgColor: {
+    type: String,
+    default: "transparent", // Default background color
+  },
+  hoverColor: {
+    type: String,
+    default: "bg-blue-800", // Default hover color
+  },
+});
+</script>
+<template>
+  <button
+    :class="`text-black ${bgColor} hover:${hoverColor} hover:text-white py-3 px-5 rounded-md`"
+  >
+    {{ text }}
+  </button>
+</template>
