ac8d38c5be70aed207a88308892b6f5d1d7d66d2
manohySr <manohyrajaonah@gmail.com>
2024-11-01 15:00:24 +0400

    wip:    dashboard

---
+++ src/layouts/Dashboard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import NavButton from "@/components/NavButton.vue";
+import Header from "./Header.vue";
+</script>
+
+<template>
+  <div class="dashboard h-screen flex flex-col">
+    <Header />
+
+    <!-- Core section -->
+    <div class="core flex-grow flex gap-x-3">
+      <div class="navigation flex flex-col pl-5">
+        <div class="nav my-2">
+          <NavButton text="Demande" />
+        </div>
+        <div class="nav my-2">
+          <NavButton text="Proformat" />
+        </div>
+        <div class="nav my-2 mt-auto">
+          <NavButton text="Deconnection" />
+        </div>
+      </div>
+
+      <div class="view flex-grow bg-gray-50 p-3">Hello</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.navigation {
+  width: 18%;
+}
+</style>
