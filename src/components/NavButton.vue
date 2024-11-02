7f83468ac39c2ce5556214e678f69a01a2497961
manohySr <manohyrajaonah@gmail.com>
2024-11-02 14:02:03 +0400

    add:    we can use icon (primeicon) now

--- src/components/NavButton.vue
+++ src/components/NavButton.vue
@@ -12,12 +12,17 @@ defineProps({
     type: String,
     default: "bg-blue-800", // Default hover color
   },
+  icon: {
+    type: String,
+    default: "",
+  },
 });
 </script>
 <template>
   <button
-    :class="`text-black ${bgColor} hover:${hoverColor} hover:text-white py-3 px-5 rounded-md`"
+    :class="`flex items-center ${bgColor} hover:${hoverColor} hover:text-white py-3 px-5 rounded-md`"
   >
+    <i v-if="icon" :class="`${icon} mr-2`"></i>
     {{ text }}
   </button>
 </template>
